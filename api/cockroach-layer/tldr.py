12a7bd7235489d4272d6982c00cf422cb399f053
PotatoParser <wilsonqnguyen@gmail.com>
2022-10-16 02:15:07 -0700

    Template TLDR

--- api/cockroach-layer/tldr.py
+++ api/cockroach-layer/tldr.py
@@ -1,6 +1,14 @@
 from util import *
+from setup import setup
 
+def tldr(text, version=0):
+  # Import text layer
+  pass
 
+def revise_tldr(text, tldr_text):
+  # Revising the text
+  pass
 
 if __name__ == "__main__":
-    main()
+  setup()
+  tldr()

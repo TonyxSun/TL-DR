feceba6bd7fdbc3f8c919ffbed4ca33073c1ed9a
Tony Sun <sunwuyue@live.com>
2022-10-16 02:06:31 -0700

    Add cohere integration

--- app.py
+++ app.py
@@ -1,23 +1,28 @@
 from flask import Flask, jsonify, redirect, url_for, request
 from dotenv import load_dotenv
 load_dotenv()
-import os
-
-# import token from .env -> `os.environ.get("api-token")`
+import os, cohere
+import nlp
 
 app = Flask(__name__)
+api_key = os.getenv('COHERE_KEY')
+co = cohere.Client(api_key)
 
 @app.route('/')
 def api(): 
     return {"up": True}
 
-@app.route('/analyse', methods = ['POST'])
+@app.route('/analyze', methods = ['POST'])
 def analyse():
     if request.method == 'POST':
+        print(request)
         user_req = request.form['user_data']
         data_req = request.form['input_data']
     
-    return {"response": "data goes here"}
+    tldr = nlp.generateSummery(co, data_req)
+    print(tldr)
+    sentiment_res = nlp.generateSentiment(co, data_req)
+    return {"tldr": tldr, "sentiment": sentiment_res.prediction}
 
 if __name__ == '__main__':
     app.run()
